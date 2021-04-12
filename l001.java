05454e15bf862be87abda352d52ff74cc637134d
prachi2399 <prachi2399@gmail.com>
2021-04-12 10:16:58 +0530

    Create l001.java

---
+++ l001.java
@@ -0,0 +1,32 @@
+public class l001{
+    // how to throw exceptions
+    public static int divideSolve(int a, int b) throws Exception{
+       if(b==0) {
+           throw new Exception("CannotDividebyZero");
+       } 
+       return a/b;
+    }
+
+    public static int divide(int a, int b){
+        int ans = -1;
+
+        try{
+            ans = divideSolve(a,b);
+        } catch (Exception e){
+            System.out.println("INFI");
+        }
+        return ans;
+    }
+    
+    public static void main(String[] args) {
+        stack st=new stack();
+        for(int i=0;i<10;i++){
+            st.push(i+10);
+        }    
+
+        while(st.size()!=0){
+            System.out.print(st.pop()+" ");
+        }
+
+    }
+}
